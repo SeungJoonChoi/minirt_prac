b79308c577e8c1d6385b0e0cf4ea0687d84dc0a2
SeungJoonChoi <tmdwns0845@naver.com>
2022-12-16 14:11:16 +0900

    221216

---
+++ camera.c
@@ -0,0 +1,20 @@
+#include "minirt.h"
+#include <math.h>
+
+//Degrees × (π/180) = Radians
+
+t_camera camera(t_vec *orig, double fov, double aspect_ratio)
+{
+    t_camera ret;
+    double theta;
+    double h;
+
+    theta = degToRad(fov);
+    h = tan(theta/2);
+    ret.viewport_height = 2.0 * h;
+    ret.viewport_width = aspect_ratio * ret.viewport_height;
+    
+    ret.orig = *orig;
+    ret.focal_length = 1.0;
+    return (ret);
+}
--- includes/minirt.h
+++ includes/minirt.h
@@ -2,6 +2,8 @@
 #define MINIRT_H
 
 #include <math.h>
+#define degToRad(angleInDegrees) ((angleInDegrees) * M_PI / 180.0)
+#define radToDeg(angleInRadians) ((angleInRadians) * 180.0 / M_PI)
 
 typedef struct s_vec
 {
@@ -15,6 +17,14 @@ typedef struct s_ray
     t_vec orig;
     t_vec dir;
 } t_ray;
+
+typedef struct s_camera
+{
+    t_vec orig;
+    double viewport_height;
+    double viewport_width;
+    double focal_length;
+} t_camera;
 
 //vec1.c
 t_vec vec(double x, double y, double z);
